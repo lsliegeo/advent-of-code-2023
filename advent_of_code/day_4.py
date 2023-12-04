f7297d13a89f2d1fd3e1db988cf7b6829206d25b
Lucas Liégeois <lucasl@live.be>
2023-12-04 09:57:50 +0100

    day 4 part 2

--- advent_of_code/day_4.py
+++ advent_of_code/day_4.py
@@ -17,19 +17,32 @@ def part1(input_data: str):
         for line in input_data.splitlines()
     ]
     return sum(
-        2 ** (number_matching - 1)
+        2 ** (number_matches - 1)
         for winning, current in cards
-        if (number_matching := len(set(winning) & set(current))) > 0
+        if (number_matches := len(set(winning) & set(current))) > 0
     )
 
 
 def part2(input_data: str):
-    pass
+    cards = [
+        (
+            [int(n) for n in line.split(': ')[1].split(' | ')[0].split()],
+            [int(n) for n in line.split(': ')[1].split(' | ')[1].split()]
+        )
+        for line in input_data.splitlines()
+    ]
+    card_number_to_amount = {i: 1 for i in range(len(cards))}
+    for i, (winning, current) in enumerate(cards):
+        for j in range(len(set(winning) & set(current))):
+            next_card_number = i + j + 1
+            if next_card_number in card_number_to_amount:
+                card_number_to_amount[next_card_number] += card_number_to_amount[i]
+    return sum(card_number_to_amount.values())
 
 
 if __name__ == '__main__':
     assert part1(EXAMPLE) == 13
     print(f'Solution for part 1 is: {part1(get_input())}')
 
-    assert part2(EXAMPLE) == None
+    assert part2(EXAMPLE) == 30
     print(f'Solution for part 2 is: {part2(get_input())}')
