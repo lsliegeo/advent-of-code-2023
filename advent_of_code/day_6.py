f41ae18a0a8461e3cd7f2760cf08775d4f7bdad9
Lucas Liégeois <lucasl@live.be>
2023-12-06 10:21:54 +0100

    mathematical solution

--- advent_of_code/day_6.py
+++ advent_of_code/day_6.py
@@ -1,3 +1,5 @@
+import math
+
 from util.input_util import get_input
 
 EXAMPLE = """Time:      7  15   30
@@ -27,9 +29,33 @@ def part2(input_data: str):
     return result
 
 
+def part2_mathematical(input_data: str):
+    duration = int(input_data.splitlines()[0].split(':')[1].replace(' ', ''))
+    distance_to_beat = int(input_data.splitlines()[1].split(':')[1].replace(' ', ''))
+
+    # f(x) = -x ** 2 + duration * x - distance_to_beat
+    # f(x) = a * x**2 + b * x + c
+    a = -1
+    b = duration
+    c = -distance_to_beat
+    discriminant = duration ** 2 - 4 * a * c
+    if discriminant <= 0:
+        raise ValueError('Unexpected discriminant')
+    x1 = (-b - math.sqrt(discriminant)) / (2 * a)
+    x2 = (-b + math.sqrt(discriminant)) / (2 * a)
+
+    x_min = min(x1, x2)
+    x_max = max(x1, x2)
+
+    return int(x_max) - int(x_min)
+
+
 if __name__ == '__main__':
     assert part1(EXAMPLE) == 288
     print(f'Solution for part 1 is: {part1(get_input())}')
+
+    assert part2_mathematical(EXAMPLE) == 71503
+    print(f'Solution for part 2 is: {part2_mathematical(get_input())}')
 
     assert part2(EXAMPLE) == 71503
     # takes ~13 sec
