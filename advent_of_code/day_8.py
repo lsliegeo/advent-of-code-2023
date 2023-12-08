98dd9b3f40390e6a6f02a449b2a13ee9dd32a1bb
Lucas Liégeois <lucasl@live.be>
2023-12-08 10:49:04 +0100

    day 8 part 2

--- advent_of_code/day_8.py
+++ advent_of_code/day_8.py
@@ -1,3 +1,4 @@
+import math
 from typing import Iterator
 
 from util.input_util import get_input
@@ -18,29 +19,48 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)"""
 
+EXAMPLE3 = """LR
 
-def part1(input_data: str):
-    network: dict[str, tuple[str, str]] = {
-        line.split(' = ')[0]: tuple(line.split(' = ')[1].strip('(').strip(')').split(', '))
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"""
+
+
+def steps_iterator(steps_str: str) -> Iterator[bool]:
+    s = [c == 'R' for c in steps_str.splitlines()[0]]
+    while 1:
+        yield from s
+
+
+def parse_network(input_data: str) -> dict[str, tuple[str, str]]:
+    return {
+        line.split(' = ')[0]: tuple(line.split(' = ')[1][1:-1].split(', '))
         for line in input_data.splitlines()[2:]
     }
 
-    def steps_iterator() -> Iterator[bool]:
-        s = [c == 'R' for c in input_data.splitlines()[0]]
-        while 1:
-            yield from s
 
+def steps_until_end(network: dict[str, tuple[str, str]], steps_str: str, current: str, until_zzz: bool) -> int:
     number_steps = 0
-    current = 'AAA'
-    steps = steps_iterator()
-    while current != 'ZZZ':
+    steps = steps_iterator(steps_str)
+    while current != 'ZZZ' if until_zzz else current[-1] != 'Z':
         current = network[current][next(steps)]
         number_steps += 1
     return number_steps
 
 
+def part1(input_data: str):
+    network = parse_network(input_data)
+    return steps_until_end(network, input_data.splitlines()[0], 'AAA', until_zzz=True)
+
+
 def part2(input_data: str):
-    pass
+    network = parse_network(input_data)
+    return math.lcm(*[steps_until_end(network, input_data.splitlines()[0], pos, until_zzz=False) for pos in network if pos[-1] == 'A'])
 
 
 if __name__ == '__main__':
@@ -48,5 +68,5 @@ if __name__ == '__main__':
     assert part1(EXAMPLE2) == 6
     print(f'Solution for part 1 is: {part1(get_input())}')
 
-    assert part2(EXAMPLE) == None
+    assert part2(EXAMPLE3) == 6
     print(f'Solution for part 2 is: {part2(get_input())}')
