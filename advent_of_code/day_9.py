34bd3cebc16eb2479f1db7853e5df137830c41e8
Lucas Liégeois <lucasl@live.be>
2023-12-09 13:02:26 +0100

    day 9 part 1

---
+++ advent_of_code/day_9.py
@@ -0,0 +1,34 @@
+from util.input_util import get_input
+
+EXAMPLE = """0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45"""
+
+
+def lag(line: list[int]) -> list[int]:
+    return [line[i + 1] - line[i] for i in range(len(line) - 1)]
+
+
+def extrapolate(line: list[int]) -> int:
+    result = 0
+    while line and any(line):
+        result += line[-1]
+        line = lag(line)
+    return result
+
+
+def part1(input_data: str):
+    lines = [list(map(int, line.split())) for line in input_data.splitlines()]
+    return sum(extrapolate(line) for line in lines)
+
+
+def part2(input_data: str):
+    pass
+
+
+if __name__ == '__main__':
+    assert part1(EXAMPLE) == 114
+    print(f'Solution for part 1 is: {part1(get_input())}')
+
+    assert part2(EXAMPLE) == None
+    print(f'Solution for part 2 is: {part2(get_input())}')
