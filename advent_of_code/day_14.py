58444e9f7738fb971a8b499cc85b80766059f7b3
Lucas Liégeois <lucasl@live.be>
2023-12-14 11:30:47 +0100

    day 14 part 2

--- advent_of_code/day_14.py
+++ advent_of_code/day_14.py
@@ -1,3 +1,6 @@
+from typing import Iterator
+
+from util.grid_utils import Coordinate, Direction
 from util.input_util import get_input
 
 EXAMPLE = """O....#....
@@ -13,36 +16,91 @@ O.#..O.#.#
 
 
 def part1(input_data: str):
-    columns = [[''] * len(input_data.splitlines()) for _ in range(len(input_data.splitlines()[0]))]
-    for y, line in enumerate(input_data.splitlines()):
-        for x, char in enumerate(line):
-            columns[x][y] = char
-
-    for column in columns:
-        previous_empty_index = None
-        for i, char in enumerate(column):
-            if char == '#':
-                previous_empty_index = None
-            elif char == '.':
-                if previous_empty_index is None:
-                    previous_empty_index = i
-            elif char == 'O':
-                if previous_empty_index is not None:
-                    column[previous_empty_index], column[i] = 'O', '.'
-                    previous_empty_index += 1
-            else:
-                raise ValueError()
-
-    return sum(len(columns) - i for column in columns for i, char in enumerate(column) if char == 'O')
+    rows = from_input_data(input_data)
+    return score(tilt(rows, Direction.NORTH))
 
 
 def part2(input_data: str):
-    pass
+    input_data_to_index = {}
+    i = 1000000000
+    while i > 0:
+        if (previous_index := input_data_to_index.get(input_data)) and i > previous_index - i:
+            i %= previous_index - i
+        else:
+            input_data_to_index[input_data] = i
+            input_data = tilt_4(input_data)
+            i -= 1
+    return score(from_input_data(input_data))
+
+
+def from_input_data(input_data: str) -> list[list[str]]:
+    return [list(line) for line in input_data.splitlines()]
+
+
+def to_input_data(rows: list[list[str]]) -> str:
+    return '\n'.join(''.join(row) for row in rows)
+
+
+def score(rows: list[list[str]]) -> int:
+    return sum(len(rows) - x for x, line in enumerate(rows) for char in line if char == 'O')
+
+
+def tilt_4(input_data: str) -> str:
+    rows = from_input_data(input_data)
+    rows = tilt(rows, Direction.NORTH)
+    rows = tilt(rows, Direction.WEST)
+    rows = tilt(rows, Direction.SOUTH)
+    rows = tilt(rows, Direction.EAST)
+    return to_input_data(rows)
+
+
+def tilt(rows: list[list[str]], direction: Direction) -> list[list[str]]:
+    prev_co = None
+    for co in iterate(rows, direction):
+        if co is None:
+            prev_co = None
+        else:
+            char = rows[co.x][co.y]
+            if char == '#':
+                prev_co = None
+            elif char == '.':
+                if prev_co is None:
+                    prev_co = co
+            else:
+                if prev_co is not None:
+                    rows[prev_co.x][prev_co.y], rows[co.x][co.y] = 'O', '.'
+                    prev_co = prev_co.step(Direction.opposite(direction))
+    return rows
+
+
+def iterate(rows: list[list], direction: Direction) -> Iterator[Coordinate | None]:
+    match direction:
+        case Direction.NORTH:
+            for y in range(len(rows[0])):
+                for x in range(len(rows)):
+                    yield Coordinate(x, y)
+                yield None
+        case Direction.SOUTH:
+            for y in range(len(rows[0])):
+                for x in reversed(range(len(rows))):
+                    yield Coordinate(x, y)
+                yield None
+        case Direction.WEST:
+            for x in range(len(rows)):
+                for y in range(len(rows[0])):
+                    yield Coordinate(x, y)
+                yield None
+        case Direction.EAST:
+            for x in range(len(rows)):
+                for y in reversed(range(len(rows[0]))):
+                    yield Coordinate(x, y)
+                yield None
 
 
 if __name__ == '__main__':
     assert part1(EXAMPLE) == 136
+    assert part1(get_input()) == 106648
     print(f'Solution for part 1 is: {part1(get_input())}')
 
-    assert part2(EXAMPLE) == None
-    print(f'Solution for part 2 is: {part2(get_input())}')
+    assert part2(EXAMPLE) == 64
+    print(f'Solution for part 2 is: {part2(get_input())}')  # takes ~8 seconds to find a cycle
