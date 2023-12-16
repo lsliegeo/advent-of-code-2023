f188271e5fd829ffc96095b14059ba821674cbe8
Lucas Liégeois <lucasl@live.be>
2023-12-16 13:15:00 +0100

    day 16 part 2

--- advent_of_code/day_16.py
+++ advent_of_code/day_16.py
@@ -1,5 +1,6 @@
 from util.grid_utils import Coordinate, Direction, ListGrid
 from util.input_util import get_input
+from util.timer_util import ContextTimer
 
 EXAMPLE = """.|...\....
 |.-.\.....
@@ -44,31 +45,46 @@ def parse_grid(input_data: str) -> ListGrid:
     return grid
 
 
-def part1(input_data: str):
-    grid = parse_grid(input_data)
-    to_explore: list[tuple[Coordinate, Direction]] = [(Coordinate(0, 0), Direction.EAST)]
+def explore(grid: ListGrid, start_co: Coordinate, start_direction: Direction) -> set[tuple[Coordinate, Direction]]:
+    to_explore: list[tuple[Coordinate, Direction]] = [(start_co, start_direction)]
     explored: set[tuple[Coordinate, Direction]] = set()
-    energized: set[Coordinate] = set()
     while to_explore:
         co, direction = to_explore.pop()
         explored.add((co, direction))
-        energized.add(co)
         char = grid[co.x][co.y]
         for next_direction in interact(direction, char):
             next_co = co.step(next_direction)
             if (next_co, next_direction) not in explored and grid.is_in_bounds(next_co):
                 to_explore.append((next_co, next_direction))
+    return explored
 
-    return len(energized)
+
+def number_energized(explored: set[tuple[Coordinate, Direction]]) -> int:
+    return len(set(co for co, _ in explored))
+
+
+def part1(input_data: str):
+    grid = parse_grid(input_data)
+    return number_energized(explore(grid, Coordinate(0, 0), Direction.EAST))
 
 
 def part2(input_data: str):
-    pass
+    grid = parse_grid(input_data)
+    best = 0
+
+    for x in range(grid.max_x + 1):
+        best = max(best, number_energized(explore(grid, Coordinate(x, 0), Direction.EAST)))
+        best = max(best, number_energized(explore(grid, Coordinate(x, grid.max_y), Direction.WEST)))
+    for y in range(grid.max_y + 1):
+        best = max(best, number_energized(explore(grid, Coordinate(0, y), Direction.SOUTH)))
+        best = max(best, number_energized(explore(grid, Coordinate(grid.max_x, y), Direction.NORTH)))
+    return best
 
 
 if __name__ == '__main__':
     assert part1(EXAMPLE) == 46
     print(f'Solution for part 1 is: {part1(get_input())}')
 
-    assert part2(EXAMPLE) == None
-    print(f'Solution for part 2 is: {part2(get_input())}')
+    assert part2(EXAMPLE) == 51
+    with ContextTimer():
+        print(f'Solution for part 2 is: {part2(get_input())}')
